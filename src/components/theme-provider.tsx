8c57850dfa5084ff1872f2ad00b5a87971418bc9
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-04-05 22:47:42 +0700

    feat: login service

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,6 @@
 
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
